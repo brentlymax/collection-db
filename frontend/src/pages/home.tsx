4594561de1218c18904a93ff89630a308c65553d
Brently Maxwell <brentlymaxwell@gmail.com>
2025-10-07 23:24:34 +0000

    Initial backend route setup

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,5 +1,30 @@
+// import React, { useEffect, useState } from 'react'
+
 function Home() {
-    return <h1>Home Page</h1>;
+	// const [data, setData] = useState(null)
+
+	// useEffect(() => {
+	// 	fetch('/api/data')
+	// 		.then((response) => response.json())
+	// 		.then((data) => setData(data))
+	// 		.catch((error) => console.error('Error fetching data: ', error))
+	// }, [])
+
+	// return (
+	// 	<div>
+	// 		{data ? (
+	// 			<ul>
+	// 				{data.map((item, index) => (
+	// 					<li key={index}>{item}</li>
+	// 				))}
+	// 			</ul>
+	// 		) : (
+	// 			<p>Loading...</p>
+	// 		)}
+	// 	</div>
+	// )
+
+	return <h1>Home Page</h1>
 }
 
-export default Home;
+export default Home
