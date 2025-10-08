515e66ae80d1ee6e6cba928711f97bdce4b67c6f
Brently Maxwell <brentlymaxwell@gmail.com>
2025-10-08 03:32:34 +0000

    Backend-dev nginx communication set up

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,7 +2,6 @@ import express from 'express'
 
 const app = express()
 const port = 3000
-const host = '0.0.0.0'
 
 app.get('/', (req, res) => {
 	res.send('Hello from the B-Man!')
@@ -12,6 +11,6 @@ app.get('/test/data', (req, res) => {
 	res.send('Test data')
 })
 
-app.listen(port, host, () => {
-	console.log(`Server is listening at http://${host}:${port}`)
+app.listen(port, () => {
+	console.log(`Server is listening at port: ${port}`)
 })
